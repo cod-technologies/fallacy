8d80bc36814beb3e9d59defe9c60062e85bddb31
David Li <davidli2010@foxmail.com>
2022-03-06 09:16:15 +0800

    Re-export alloc

---
+++ src/alloc.rs
@@ -0,0 +1,4 @@
+//! Memory allocation & deallocation.
+
+pub use core::alloc::{AllocError, Allocator, Layout, LayoutError};
+pub use std::alloc::Global;
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
 //! A library for fallible allocations, collections and operations.
+
+#![feature(allocator_api)]
+
+pub mod alloc;
